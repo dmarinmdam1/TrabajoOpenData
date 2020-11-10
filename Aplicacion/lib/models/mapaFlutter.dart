a90774bdaf43f22f207a877066d1177872dd05e0
dmarinmdam1 <dmarinmdam1@gmail.com>
2020-11-10 13:36:11 +0100

    Avances varios

--- Aplicacion/lib/models/mapaFlutter.dart
+++ Aplicacion/lib/models/mapaFlutter.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 class MapaFlutter extends StatefulWidget {
   @override
@@ -8,8 +10,36 @@ class MapaFlutter extends StatefulWidget {
 class _MapaFlutterState extends State<MapaFlutter> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("mapaFlutter"),
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(42.825537, -1.632080),
+        zoom: 16.0,
+      ),
+      layers: [
+        TileLayerOptions(urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", subdomains: [
+          'a',
+          'b',
+          'c'
+        ]),
+        MarkerLayerOptions(
+          markers: [
+            /*
+            Marker(
+              width: 80.0,
+              height: 80.0,
+              point: LatLng(42.825541, -1.632107),
+              builder: (ctx) => Container(
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: Colors.blue,
+                  size: 50.0,
+                ),
+              ),
+            ),
+          */
+          ],
+        ),
+      ],
     );
   }
 }
